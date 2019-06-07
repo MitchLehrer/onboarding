08dfdb171bfb461d6efba36ca387db31fc10dc74
Mitch Lehrer <mitchell@vivvo.com>
2019-06-07 11:48:35 -0600

    BFF Setup And Controllers Creation

--- onboarding-service/src/main/java/com/vivvo/OnboardingApplication.java
+++ onboarding-service/src/main/java/com/vivvo/OnboardingApplication.java
@@ -8,18 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.MessageSourceAccessor;
 
 @SpringBootApplication
-public class OnboadingApplication {
-
-    //TODO implement find for users
-    // use twillio to send an sms code and verify a phone number (more actions)
-
-
+public class OnboardingApplication {
 
     @Autowired
     private MessageSource messageSource;
 
     public static void main(String[] args) {
-        SpringApplication.run(OnboadingApplication.class, args);
+        SpringApplication.run(OnboardingApplication.class, args);
     }
 
     @Bean
---
+++ onboarding-webapp/src/main/java/com/vivvo/BFF.java
@@ -0,0 +1,25 @@
+package com.vivvo;
+
+import com.vivvo.onboarding.UserClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BFF {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BFF.class, args);
+    }
+
+    @Bean
+    public UserClient userClient() {
+        return new UserClient();
+    }
+}
+
+
+
+
+
